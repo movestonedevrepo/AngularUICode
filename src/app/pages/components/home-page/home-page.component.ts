69b488a9146fd7fc4af08838d5a7d3662bcbf5a7
Soumyajit Sarkar <Soumyajit.Sarkar@hyland.com>
2024-01-02 17:42:25 +0530

    Angular upgrade

--- src/app/pages/components/home-page/home-page.component.ts
+++ src/app/pages/components/home-page/home-page.component.ts
@@ -10,7 +10,7 @@ import {
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css'],
+  styleUrl: './home-page.component.css',
   encapsulation: ViewEncapsulation.None,
 })
 export class HomePageComponent {
