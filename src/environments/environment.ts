913ad663de1998f42f91d160a9e26dea7d6dd8bf
Shubhayu Dasgupta <MY_NAME@example.com>
2024-03-06 21:57:36 +0530

    environment changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   assestsBasePath: '../../../assets/',
+  // baseUrl: 'https://movestonebackend.onrender.com',
   baseUrl: 'https://movestonebackend.onrender.com',
   //baseUrl:'http://localhost:5000'
 };
