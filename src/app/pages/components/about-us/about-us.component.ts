eaa56a8231f118399d1bcd234f25e150506c0949
Soumyajit Sarkar <Soumyajit.Sarkar@hyland.com>
2024-03-11 17:27:35 +0530

    About us page design fixed

--- src/app/pages/components/about-us/about-us.component.ts
+++ src/app/pages/components/about-us/about-us.component.ts
@@ -1,12 +1,63 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.css'
+  styleUrl: './about-us.component.css',
 })
 export class AboutUsComponent {
-
+  teamMembers = [
+    {
+      image:
+        'https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/aw40.png',
+      name: ' Roger Rubin',
+      position: 'Lorem ipsum',
+      about:
+        ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+    },
+    {
+      image:
+        'https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/aw42.png',
+      name: ' Roger Rubin',
+      position: 'Lorem ipsum',
+      about:
+        ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+    },
+    {
+      image:
+        'https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/aw41.png',
+      name: ' Roger Rubin',
+      position: 'Lorem ipsum',
+      about:
+        ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+    },
+    {
+      image:
+        'https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/aw41.png',
+      name: ' Roger Rubin',
+      position: 'Lorem ipsum',
+      about:
+        ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+    },
+    {
+      image:
+        'https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/aw40.png',
+      name: ' Roger Rubin',
+      position: 'Lorem ipsum',
+      about:
+        ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+    },
+    {
+      image:
+        'https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/aw42.png',
+      name: ' Roger Rubin',
+      position: 'Lorem ipsum',
+      about:
+        ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+    },
+  ];
 }
--- src/app/pages/pages.routes.ts
+++ src/app/pages/pages.routes.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 import { getQueryResolver } from '../resolvers/get-query.resolver';
 import { homeDetailsResolver } from '../resolvers/home-details.resolver';
 import { productSpecResolver } from '../resolvers/product-spec.resolver';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 import { AllProductsComponent } from './components/all-products/all-products.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { QueryViewComponent } from './components/query-view/query-view.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
 
 export const PAGES_ROUTES: Routes = [
   {
@@ -17,8 +17,8 @@ export const PAGES_ROUTES: Routes = [
     redirectTo: 'home',
   },
   {
-    path:'about',
-    component:AboutUsComponent
+    path: 'about',
+    component: AboutUsComponent,
   },
   {
     path: 'home',
