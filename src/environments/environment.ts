409eb84da15e82bb08cbd91e5990cf0519969c3d
Soumyajit Sarkar <Soumyajit.Sarkar@hyland.com>
2024-06-05 11:34:38 +0530

    New Design

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: false,
   assestsBasePath: '../../../assets/',
   baseUrl: 'https://movestonebackend.onrender.com',
+  // baseUrl:'http://localhost:5000'
 };
