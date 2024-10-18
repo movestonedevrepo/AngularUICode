79cfb046833319d3ac43dc7f968f48d142b95282
Soumyajit Sarkar <soumyajit.sarkar@hyland.com>
2024-10-18 13:36:02 +0530

    Environment created for prod

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   assestsBasePath: '../../../assets/',
-  baseUrl: 'http://localhost:8080',
+  baseUrl: 'https://rickshawback.onrender.com'
 };
